83d71738696b60b61da85fa4d51d7787f009ffb3
jerryandersom <zohaibsaddiqui5@gmail.com>
2024-07-17 10:37:34 -0400

    search api done

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
+const { loadData } = require("./util/import-mongo/index");
 
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
@@ -29,11 +29,12 @@ app.use(express.json());
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
 //{{insert code here}}
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
-app.use('/api/secondchance/items', secondChanceItemsRoutes);
-
+const searchRoutes = require('./routes/searchRoutes');
+app.use('/api/secondchance/search', searchRoutes);
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -48,7 +49,6 @@ app.use(pinoHttp({ logger }));
 //{{insert code here}}
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
 
 
 // Global Error Handler
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Inside the server")
 })
 
