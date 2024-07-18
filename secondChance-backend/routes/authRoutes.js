af4c33aebfc15471b6627a94f4c3f43515718f5b
jerryandersom <zohaibsaddiqui5@gmail.com>
2024-07-18 17:29:15 -0400

    Implemented API endpoints

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const logger = require('../logger');
-const { JWT_SECRET } = process.env;
-
-require('dotenv').config();
+const connectToDatabase = require('../models/db');
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
+dotenv.config();
+const logger = pino();  // Create a Pino logger instance
+//Create JWT secret
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
